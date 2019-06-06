fd7f85e8bc637c2a2521b5f80fce8620c0db0152
adam-brendan <adamcraveiro@gmail.com>
2019-06-06 16:28:14 +0000

    Implemenet support for image messages

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,22 +4,21 @@ function Message(props) {
     
     function isImage(message) {
         const imgExtensions = [".jpg", ".jpeg", ".png", ".gif"]
-        imgExtensions.forEach(function(extension) {
-            if (props.message.content.includes(extension)) {
-                return true;
-            }
-            return false;
-        })
+        const messageExtension = message.substring(message.lastIndexOf("."));
+        return imgExtensions.includes(messageExtension);
     }
 
     return (
         <div className="messageSystem">
             <div className="message">
-                {props.message.type === "incomingMessage" ?
-                    <div><span style={{color: props.message.userColor}} className="messageUsername"><strong>{props.message.username}:</strong></span>
-                    <span className="messageContent"> {props.message.content}</span></div> :
-                    <span className="messageContent"><em>{props.message.content}</em></span>     
-                }
+            {(!isImage(props.message.content) && props.message.type === "incomingMessage") ?
+                <div><span style={{color: props.message.userColor}} className="messageUsername"><strong>{props.message.username}:</strong></span>
+                <span className="messageContent"> {props.message.content}</span></div> 
+                : (isImage(props.message.content) && props.message.type === "incomingMessage") ?
+                <div><span style={{color: props.message.userColor}} className="messageUsername"><strong>{props.message.username}:</strong></span>
+                <img className="messagePic" src={props.message.content} alt="User sent an image"/></div>
+                : <span className="messageContent"><em>{props.message.content}</em></span>     
+            }
             </div>
         </div>
     )
